755724295367a898c4f384112f5fe48c42374a73
Surgeom <ushakevichkirill@gmail.com>
2021-07-01 16:20:02 +0300

    les4

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'geekshop.urls'
+AUTH_USER_MODEL='authapp.ShopUser'
 
 TEMPLATES = [
     {
